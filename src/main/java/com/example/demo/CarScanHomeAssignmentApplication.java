0e104042ae8a28e2a730f9edc3a0f9e7146fa4e3
Anmol Chhabra <anmolchhabra098@gmail.com>
2022-01-14 14:55:18 +0530

    Changed data in data.sql, Added meta data in main class for documentation

--- src/main/java/com/example/demo/CarScanHomeAssignmentApplication.java
+++ src/main/java/com/example/demo/CarScanHomeAssignmentApplication.java
@@ -2,6 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 
 @SpringBootApplication
 public class CarScanHomeAssignmentApplication {
@@ -10,4 +15,11 @@ public class CarScanHomeAssignmentApplication {
 		SpringApplication.run(CarScanHomeAssignmentApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("CarScan take home assignment by Anmol Chhabra")
+						.description("Vehicle inspection technology in the palm of your hand. A mobile app that's the first of its kind in the motor industry, Carscan uses computer vision and machine learning to scan vehicle damages and provide real time vehicle health condition and repair estimates.")
+						.version("v0.0.1"));
+	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into car_scan_users (mobile_number,fname,lname,dob,city) values ('9822398740','Ram','Malhotra','19-02-1990','Panipat');
-insert into car_scan_users (mobile_number,fname,lname,dob,city) values ('7745398741','Ajay','Kediya','29-12-1978','Manali');
-insert into car_scan_users (mobile_number,fname,lname,dob,city) values ('8997398742','Shivam','Kumar','05-02-2001','Noida');
-insert into car_scan_users (mobile_number,fname,lname,dob,city) values ('7688398743','Rahul','Jaykar','12-02-1985','Delhi');
-insert into car_scan_users (mobile_number,fname,lname,dob,city) values ('9654398744','Amriptal','singh','19-05-1997','Amritsar');
+insert into car_scan_users (mobile_number,fname,lname,dob,city) values ('9822398740','AVADHUT','JOSHI','19-02-1990','Mumbai');
+insert into car_scan_users (mobile_number,fname,lname,dob,city) values ('7745398741','PAYAL','GUPTA','29-12-1995','Johannesburg');
+insert into car_scan_users (mobile_number,fname,lname,dob,city) values ('8997398742','CHANDER','PRAKASH','05-02-2001','Johannesburg');
+insert into car_scan_users (mobile_number,fname,lname,dob,city) values ('7688398743','OBINS','CHOUDHARY','12-02-1988','Johannesburg');
+insert into car_scan_users (mobile_number,fname,lname,dob,city) values ('9654398744','Anmol','Chhabra','19-05-1998','Panipat');
